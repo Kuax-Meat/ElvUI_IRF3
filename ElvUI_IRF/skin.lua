53b3ad457fdd0918dd02ea70bdc92519fef42575
JaeHyeong Sim <escorsian@naver.com>
2011-09-25 22:39:02 +0900

    오늘은 여기까지 JJIK

--- ElvUI_IRF/skin.lua
+++ ElvUI_IRF/skin.lua
@@ -11,38 +11,36 @@ print("IRF3 Completely Loaded")
 
 local _G = _G
 local IRF3 = InvenRaidFrames3
-local widthvalue, heightvalue = 0, 0
-
+local LBDB = LibStub("LibBlueDB-1.0")
 IRF3:RegisterEvent("PLAYER_LOGIN")
+
+------------------
+-- Movable Code --
+------------------
+local MovableRaid = CreateFrame("Frame",nil,UIParent)
+MovableRaid:CreatePanel("Default",64,39,"CENTER", UIParent, "CENTER", 0, 0)
+MovableRaid:Hide()
+E.CreateMover(MovableRaid, "MovableRaid", "Move IRF")
 
 ---------------
 -- Main Skin --
 ---------------
 
-function IRF3:SetupAll(update)
-	for _, header in pairs(IRF3.headers) do
-		for _, member in pairs(header.members) do
-			member:Setup()
-			IRF3:CreateElvUI(member)
-		end
+function IRF3:LoadPosition()
+	self:SetUserPlaced(nil)
+	self:SetScale(self.db.scale)
+	self:ClearAllPoints()
+	self:SetPoint(self.db.anchor, MovableRaid, "TOPLEFT", 0, 0)
+	self.petHeader:SetUserPlaced(nil)
+	self.petHeader:SetScale(self.db.petscale)
+	self.petHeader:ClearAllPoints()
+	if self.db.petpx then
+		self.petHeader:SetPoint(self.db.petanchor, UIParent, self.db.petanchor, self.db.petpx / (self.db.petscale * self.db.scale), self.db.petpy / (self.db.petscale * self.db.scale))
+	else
+		self.petHeader:SetPoint(self.db.petanchor, UIParent, "CENTER", 0, 0)
 	end
-	for _, member in pairs(IRF3.petHeader.members) do
-		member:Setup()
-		IRF3:CreateElvUI(member)
-	end
-	self.db.units.texture = "ElvUI Norm"
-	self.db.font.file = "ElvUI Font"
-	self.db.font.size = 10
-	self.db.font.attribute = "THINOUTLINE"
-	self.db.font.shadow = false
-	self.db.units.powerBarHeight = 0.1
-	self.db.border = false
-	self.db.partyTag = false
-	self.db.colors.MANA = { 0.31, 0.45, 0.63 }
-	self.db.colors.RAGE = { 0.78, 0.25, 0.25 }
-	self.db.usePet = false
-	self.db.offset = 6
-	self.db.units.className = true
+	local widthvalue, heightvalue = 0, 0
+	
 	if (E.screenwidth / E.screenheight) == 1.6 then
 		if IsAddOnLoaded("Elvui_RaidDPS") then
 			widthvalue = ((ChatLBGDummy:GetWidth() - (27)) / 5)
@@ -56,13 +54,60 @@ function IRF3:SetupAll(update)
 			widthvalue = ((ElvuiActionBarBackground:GetWidth() - (28)) / 5)
 		end
 	end
-	print(widthvalue)
+
 	self.db.width = widthvalue
 	self.db.height = E.Scale(39)
+	self.db.lock = true
+	
+	self.db.dir = 2
+	self.db.border = false
+	self.db.partyTag = false
+	self.db.usePet = false
+	self.db.offset = 6
+	self.db.groupshown = { true, true, true, true, true, false, false, false }
+	self.db.column = 1
+
+	self.db.units.powerBarHeight = 0.1
+	self.db.units.healIcon = false
+	self.db.units.healIconOther = false
+	self.db.units.raidIconPos = "TOP"
+	self.db.units.raidIconSize = 15
+	self.db.units.className = true
+	self.db.units.bossDebuffSize = 23
+	self.db.units.bossDebuffPos = "BOTTOMRIGHT"
+	self.db.units.bossDebuffAlpha = 1
+	self.db.units.bossDebuffTimer = false
+	self.db.units.fadeOutOfRangePower = true
+	self.db.units.fadeOutAlpha = 0.3
+	self.db.units.texture = "ElvUI Norm"
+	--self.db.units.tooltip = 1
+	--self.db.units.healthType = 0
+	self.db.units.useCastingBar = false
+	self.db.units.usePowerBarAlt = false
+	
+	self.db.font.file = "ElvUI Font"
+	self.db.font.size = 10
+	self.db.font.attribute = "THINOUTLINE"
+	self.db.font.shadow = false
+	
+	self.db.colors.offline = E.colors.disconnected
+	self.db.colors.MANA = { 0.31, 0.45, 0.63 }
+	self.db.colors.RAGE = { 0.78, 0.25, 0.25 }
 end
 
-function IRF3:CreateElvUI(self)
-	if not self.bdrtop then
+function IRF3:SetupAll(update)
+	for _, header in pairs(IRF3.headers) do
+		for _, member in pairs(header.members) do
+			setupMemberTexture(member)
+		end
+	end
+	for _, member in pairs(IRF3.petHeader.members) do
+		setupMemberTexture(member)
+	end
+end
+
+function setupMemberTexture(self)
+	if self.bdrtop == nil then
 		self.bdrtop = CreateFrame('Frame', nil, self.healthBar)
 		self.bdrtop:SetTemplate("Default")
 		self.bdrtop:Point("TOPRIGHT", self.healthBar, "TOPRIGHT", 2*E.ResScale, 2*E.ResScale)
@@ -92,6 +137,61 @@ function IRF3:CreateElvUI(self)
 	self.name:SetSpacing(4)
 end
 
+function InvenRaidFrames3Member_UpdateOutline(self)	-- Must delete original outline and change elvui border frame color
+	local borderr, borderg, borderb = unpack(C["media"].bordercolor)	-- import default bordercolor
+	self.outline:Hide() -- do not need this option anymore
+	
+	if self.optionTable.outline.type == 1 then	-- Ìï¥Ï†ú Í∞ÄÎä•Ìïú ÎîîÎ≤ÑÌîÑ; Also check aggro, order 1st. dispel Border otherwise Aggro color
+		if self.dispelType and IRF3.db.colors[self.dispelType] then
+			self.bdrtop:SetBackdropBorderColor(IRF3.db.colors[self.dispelType][1], IRF3.db.colors[self.dispelType][2], IRF3.db.colors[self.dispelType][3])
+			self.bdrbot:SetBackdropBorderColor(IRF3.db.colors[self.dispelType][1], IRF3.db.colors[self.dispelType][2], IRF3.db.colors[self.dispelType][3])
+			return
+		elseif self.hasAggro then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.aggroColor[1], self.optionTable.outline.aggroColor[2], self.optionTable.outline.aggroColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.aggroColor[1], self.optionTable.outline.aggroColor[2], self.optionTable.outline.aggroColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 2 then	-- ÎåÄÏÉÅ
+		if UnitIsUnit(self.displayedUnit, "target") then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.targetColor[1], self.optionTable.outline.targetColor[2], self.optionTable.outline.targetColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.targetColor[1], self.optionTable.outline.targetColor[2], self.optionTable.outline.targetColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 3 then	-- ÎßàÏö∞Ïä§ Ïò§Î≤Ñ
+		if UnitIsUnit(self.displayedUnit, "mouseover") then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.mouseoverColor[1], self.optionTable.outline.mouseoverColor[2], self.optionTable.outline.mouseoverColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.mouseoverColor[1], self.optionTable.outline.mouseoverColor[2], self.optionTable.outline.mouseoverColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 4 then	-- Ï≤¥Î†• ÎÇÆÏùå(ÌçºÏÑºÌä∏)
+		if not UnitIsDeadOrGhost(self.displayedUnit) and (self.health / self.maxHealth) <= self.optionTable.outline.lowHealth then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.lowHealthColor[1], self.optionTable.outline.lowHealthColor[2], self.optionTable.outline.lowHealthColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.lowHealthColor[1], self.optionTable.outline.lowHealthColor[2], self.optionTable.outline.lowHealthColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 5 then	-- Ïñ¥Í∑∏Î°ú
+		if self.hasAggro then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.aggroColor[1], self.optionTable.outline.aggroColor[2], self.optionTable.outline.aggroColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.aggroColor[1], self.optionTable.outline.aggroColor[2], self.optionTable.outline.aggroColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 6 then	-- Ï†ÑÏà†Î™©Ìëú ÏïÑÏù¥ÏΩò
+		if self.optionTable.outline.raidIcon[GetRaidTargetIndex(self.displayedUnit)] then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.raidIconColor[1], self.optionTable.outline.raidIconColor[2], self.optionTable.outline.raidIconColor[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.raidIconColor[1], self.optionTable.outline.raidIconColor[2], self.optionTable.outline.raidIconColor[3])
+			return
+		end
+	elseif self.optionTable.outline.type == 7 then	-- Ï≤¥Î†• ÎÇÆÏùå(Ïã§ÏàòÏπò)
+		if not UnitIsDeadOrGhost(self.displayedUnit) and self.maxHealth >= self.optionTable.outline.lowHealth2 and self.health < self.optionTable.outline.lowHealth2 then
+			self.bdrtop:SetBackdropBorderColor(self.optionTable.outline.lowHealthColor2[1], self.optionTable.outline.lowHealthColor2[2], self.optionTable.outline.lowHealthColor2[3])
+			self.bdrbot:SetBackdropBorderColor(self.optionTable.outline.lowHealthColor2[1], self.optionTable.outline.lowHealthColor2[2], self.optionTable.outline.lowHealthColor2[3])
+			return
+		end
+	end
+	-- reset bordercolor
+	self.bdrtop:SetBackdropBorderColor(borderr, borderg, borderb)
+	self.bdrbot:SetBackdropBorderColor(borderr, borderg, borderb)
+end
 ---------------
 -- ICON SKIN --
 ---------------
@@ -214,21 +314,21 @@ InvenRaidFrames3Member_UpdateDisplayText_ = InvenRaidFrames3Member_UpdateDisplay
 InvenRaidFrames3Member_UpdateDisplayText = function(self)
 	if self.isOffline then
 		if IRF3.db.units.nameEndl then
-			self.name:SetFormattedText("%s\n|cffD7BEA5ø¿«¡∂Û¿Œ|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ïò§ÌîÑÎùºÏù∏|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		else
-			self.name:SetFormattedText("%s\n|cffD7BEA5ø¿«¡∂Û¿Œ|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ïò§ÌîÑÎùºÏù∏|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		end
 	elseif self.isGhost then
 		if IRF3.db.units.nameEndl then
-			self.name:SetFormattedText("%s\n|cffD7BEA5¿Ø∑…|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ïú†Î†π|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		else
-			self.name:SetFormattedText("%s\n|cffD7BEA5¿Ø∑…|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ïú†Î†π|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		end
 	elseif self.isDead then
 		if IRF3.db.units.nameEndl then
-			self.name:SetFormattedText("%s\n|cffD7BEA5¡◊¿Ω|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ï£ΩÏùå|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		else
-			self.name:SetFormattedText("%s\n|cffD7BEA5¡◊¿Ω|r", getCuttingName(self.nameTable, IRF3.nameWidth))
+			self.name:SetFormattedText("%s\n|cffD7BEA5Ï£ΩÏùå|r", getCuttingName(self.nameTable, IRF3.nameWidth))
 		end
 	else
 		if self.lostHealth > 0 and IRF3.db.units.healthType ~= 0 and checkRange(self) then
